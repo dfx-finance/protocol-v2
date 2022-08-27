fc67e0c278cb3cd153176409036de2acd762bcbc
Ubuntu <user@ubuntu.com>
2022-08-27 16:37:30 -0400

    Will have to cleanup only works on Ethereum and Eth testnets right now

--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -2,24 +2,26 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/CurveFactory.sol";
+import "../src/CurveFactoryV2.sol";
 import "../src/AssimilatorFactory.sol";
+import "../src/Ablah.sol";
 
 contract ContractScript is Script {
-    int128 protocolFee_ = 50_000; // 50%
-    address dfxTreasury_ =  0x26f539A0fE189A7f228D7982BF10Bc294FA9070c;
+    // int128 protocolFee_ = 50_000; // 50%
+    // address dfxTreasury_ =  0x80D27bfb638F4Fea1e862f1bd07DEa577CB77D38;
     
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
 
-        AssimilatorFactory assimilatorFactory = new AssimilatorFactory();
-        CurveFactory curvefactoryv2 = new CurveFactory(
+        Ablah blah = new Ablah();
+        // AssimilatorFactory assimilatorFactory = new AssimilatorFactory();
+        // CurveFactoryV2 curvefactoryv2 = new CurveFactoryV2(
             // protocolFee_,
             // dfxTreasury_,
             // address(assimilatorFactory)
-        );
+        // );
         
         vm.stopBroadcast();
     }
