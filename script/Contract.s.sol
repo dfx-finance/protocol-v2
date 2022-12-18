7e11d6a36d5b98591ccafe0c284bceba0e1f6ebb
Ubuntu <user@ubuntu.com>
2022-12-18 18:52:33 -0500

    Testing forge scripting

--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -3,10 +3,29 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-contract ContractScript is Script {
-    function setUp() public {}
+// Libraries
+import "../src/Curve.sol";
+import "../src/Curve.sol";
+import "../src/Curve.sol";
+import "../src/Curve.sol";
 
-    function run() public {
-        vm.broadcast();
+// Factories
+import "../src/CurveFactoryV2.sol";
+
+contract ContractScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        address POLYGON_MULTISIG = 0x80D27bfb638F4Fea1e862f1bd07DEa577CB77D38;
+
+        AssimilatorFactory deployedAssimFactory = new AssimilatorFactory();
+
+        // CurveFactoryV2 deployedCurveFactory = new CurveFactoryV2(
+        //     50_000,
+        //     POLYGON_MULTISIG
+            
+        // );
+
+        vm.stopBroadcast();
     }
 }
