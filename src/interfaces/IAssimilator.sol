d66e885af2e3bac1e5cfe480b307e2aa5a4f7968
tommy <tommychan4241@gmail.com>
2023-01-17 23:53:29 +0800

    9

--- src/interfaces/IAssimilator.sol
+++ src/interfaces/IAssimilator.sol
@@ -20,7 +20,9 @@ interface IAssimilator {
 
     function intakeRaw(uint256 amount) external returns (int128);
 
-    function intakeRawAndGetBalance(uint256 amount) external returns (int128, int128);
+    function intakeRawAndGetBalance(uint256 amount)
+        external
+        returns (int128, int128);
 
     function intakeNumeraire(int128 amount) external returns (uint256);
 
@@ -33,9 +35,13 @@ interface IAssimilator {
 
     function outputRaw(address dst, uint256 amount) external returns (int128);
 
-    function outputRawAndGetBalance(address dst, uint256 amount) external returns (int128, int128);
+    function outputRawAndGetBalance(address dst, uint256 amount)
+        external
+        returns (int128, int128);
 
-    function outputNumeraire(address dst, int128 amount) external returns (uint256);
+    function outputNumeraire(address dst, int128 amount)
+        external
+        returns (uint256);
 
     function viewRawAmount(int128) external view returns (uint256);
 
@@ -56,7 +62,10 @@ interface IAssimilator {
 
     function viewNumeraireBalance(address) external view returns (int128);
 
-    function viewNumeraireAmountAndBalance(address, uint256) external view returns (int128, int128);
+    function viewNumeraireAmountAndBalance(address, uint256)
+        external
+        view
+        returns (int128, int128);
 
-    function transferFee(int128, address) external returns (bool);
+    function transferFee(int128, address) external;
 }
