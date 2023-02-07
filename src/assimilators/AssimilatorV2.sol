1d64d0742b6fb8ceb8961e8b01334b221903612d
chanharryhk <chanharryhk@gmail.com>
2023-02-07 11:48:50 -0500

    Deploying now

--- src/assimilators/AssimilatorV2.sol
+++ src/assimilators/AssimilatorV2.sol
@@ -30,7 +30,7 @@ contract AssimilatorV2 is IAssimilator {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IERC20 public constant usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    IERC20 public constant usdc = IERC20(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
 
     IOracle public immutable oracle;
     IERC20 public immutable token;
